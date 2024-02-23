9d61e036d4c0e74405a45a88cc951ef9aaafaf51
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-02-23 03:43:44 -0800

    Q-07 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-07

---
+++ Q7.js
@@ -0,0 +1,10 @@
+"use strict";
+// 7.	Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Addition
+let a = 9;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
---
+++ Q7.ts
@@ -0,0 +1,28 @@
+// 7.	Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+
+// Addition
+console.log("Addition:", 5 + 3); // Output: 8
+
+// Subtraction
+console.log("Subtraction:", 10 - 2); // Output: 8
+
+// Multiplication
+console.log("Multiplication:", 4 * 2); // Output: 8
+
+// Division
+console.log("Division:", 16 / 2); // Output: 8
+
+
+// Addition
+let a: number = 9
+let b: number = 3
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+
+
